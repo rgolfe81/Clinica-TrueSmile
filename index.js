cd7b7abef681389f1b49b6f7f4e82f79bfff847d
Ruben <rgolfe81@gmail.com>
2023-03-20 01:10:04 +0100

    fix: Edit index.js with corsOptions for connecting with frontend with the same computer

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const db = require("./db/db");
 require('dotenv').config();
-
+const cors = require ("cors");
 const PORT = process.env.PORT || 3000;
 const router = require('./router');
 
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(router);
 
 
