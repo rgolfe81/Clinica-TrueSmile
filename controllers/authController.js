448265a3546a2c58791f106095b34ad880321c63
Ruben <rgolfe81@gmail.com>
2023-03-24 19:59:23 +0100

    fix: new reload

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,7 +72,7 @@ authController.login = async (req, res) => {
             message: "Login successfully",
             token: token,
             name: user.name,
-            doctorId: user.Doctor.id
+            doctorId: user.doctor.id
         }
     )
 
